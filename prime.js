fd220ae8951b6d29518b9cb1e21bc333a65b9365
himanshukrkeshri <keshripawan234@gmail.com>
2023-08-28 16:36:47 +0530

    prime number

---
+++ prime.js
@@ -0,0 +1,18 @@
+let number = 4;
+let fact = 0;
+for(let i =1; i<number;i++)
+{
+   if(number %1 == 0)
+{
+        fact++
+}
+}
+if(fact ==2)
+{
+console.log("prime")
+}
+else
+{
+console.log("not prime")
+}
+}
